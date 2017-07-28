8ce70c3c382034bc64c6c13e18bba3a3aecbf334
Paula <paula.poncez@usach.cl>
2017-07-28 16:38:04 -0400

    panel de imágenes

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	json.forEach(function(element){
+		$("#img-container").append("<div class='card'><div class='card-image'><img src='assets/img/" + element.image_url + "'></div><div class='card-content'><span class='card-title'>" + element.title + "</span><p><span>" + element.username + "</span><span>#" + element.hashtag + "</span></p></div></div>");
+	});
+});
+
+
+var elem = document.querySelector("#img-container");
+var msnry = new Masonry(elem, {
+	// options
+	itemSelector: ".card"
+});
+
+/*
+<div class='card'>
+	<div class='card-image'>
+		<img src=''>
+	</div>
+	<div class='card-content'>
+		<span class='card-title'></span>
+		<p><span></span></p>
+	</div>
+</div>
+*/
