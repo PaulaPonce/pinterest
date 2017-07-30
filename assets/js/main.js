75af4d5a2f2fa7e340fae9f725230c1929e82c5d
Paula <paula.poncez@usach.cl>
2017-07-30 16:34:55 -0400

    flex container en panel de imágenes y cambios en nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,7 @@
 $(document).ready(function() {
 	json.forEach(function(element){
-		$("#img-container").append("<div class='card'><div class='card-image'><img src='assets/img/" + element.image_url + "'></div><div class='card-content'><span class='card-title'>" + element.title + "</span><p><span>" + element.username + "</span><span>#" + element.hashtag + "</span></p></div></div>");
+		$("#img-container").append("<div class='card'><div class='card-image'><img src='assets/img/" + element.image_url + "'></div><div class='card-content'><span class='card-title'>" + element.title + "</span><p><span>" + element.username + "</span><span> #" + element.hashtag + "</span></p></div></div>");
 	});
-});
-
-
-var elem = document.querySelector("#img-container");
-var msnry = new Masonry(elem, {
-	// options
-	itemSelector: ".card"
 });
 
 /*
